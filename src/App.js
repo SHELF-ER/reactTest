5ab26a1cca9e120062a1bc7dfe4f9c55c6daae3b
woogi <dbwhddnr00@naver.com>
2022-06-16 17:54:29 +0900

    react test

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import logo from './logo.svg'; 
+import './App.css'; 
+import { useEffect, useState } from 'react'; 
+
+function App() { 
+  const [message, setMessage] = useState("")   
+  
+  useEffect(() => { 
+    fetch("/books/return/C5 F3 AF 75")
+      .then(res => res.text())
+      .then(m => setMessage(m)) 
+  }, [])   
+    
+  return (
+    <div className="App">      
+      <header className="App-header">       
+        <img src={logo} className="App-logo" alt="logo" />       
+        <p>         
+            {message}        
+        </p>        
+        <a 
+          className="App-link"
+            href="https://reactjs.org"
+            target="_blank" 
+            rel="noopener noreferrer"        
+          >          
+            Learn React        
+          </a>      
+        </header>    
+      </div>
+    );
+  } 
+export default App;
