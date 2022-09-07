a47395474e0337fb71e289117f0709676b9e315d
Jihee Eom <65202243+bonjenny@users.noreply.github.com>
2022-09-07 22:41:13 +0900

    9/7-basic create-react-app test

--- src/App.js
+++ src/App.js
@@ -1,33 +1,9 @@
-import logo from './logo.svg'; 
-import './App.css'; 
-import { useEffect, useState } from 'react'; 
-
-function App() { 
-  const [message, setMessage] = useState("")   
-  
-  useEffect(() => { 
-    fetch("/books/return/C5 F3 AF 75")
-      .then(res => res.text())
-      .then(m => setMessage(m)) 
-  }, [])   
-    
+function App() {
   return (
-    <div className="App">      
-      <header className="App-header">       
-        <img src={logo} className="App-logo" alt="logo" />       
-        <p>         
-            {message}        
-        </p>        
-        <a 
-          className="App-link"
-            href="https://reactjs.org"
-            target="_blank" 
-            rel="noopener noreferrer"        
-          >          
-            Learn React        
-          </a>      
-        </header>    
-      </div>
-    );
-  } 
+    <div>
+      <h2>Let's get started!</h2>
+    </div>
+  );
+}
+
 export default App;
